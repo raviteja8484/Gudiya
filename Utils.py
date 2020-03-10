a32c008eadbc1cfd2087074b94926afd7a4d1b17
raviteja8484 <38815388+raviteja8484@users.noreply.github.com>
2020-03-10 20:21:18 +0530

    Add files via upload

---
+++ Utils.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+
+
+
+# Function to plot the image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+
+# Code to check total accuracy
+def total_accuracy(testloader, model, device):
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+
+#Code to print classwise Accuracy
+def class_accuracy(testloader, model,device):
+    classes = ('plane', 'car', 'bird', 'cat',
+               'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
+
+
+
+def model_call(model, device, trainloader, optimizer,testloader, epoch):
+    train(model, device, trainloader, optimizer, epoch)
+    scheduler.step()
+    test(model, device, testloader)
---
+++ __init__.py
@@ -0,0 +1,5 @@
+from Gudiya.Model import Resnet,BasicBlock,Bottleneck
+from Gudiya.Model import Resnet18
+from Model_test import test
+from Model_train import train
+from Utils import *
